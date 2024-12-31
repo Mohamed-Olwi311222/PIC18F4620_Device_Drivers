94f2d92bed63bbdd53dad01c47189c7984a524e0
Mohamed-Olwi311222 <mohamedolwi38@gmail.com>
2024-12-31 20:10:01 +0200

    Added a template for I2C Module

---
+++ Application/Application.X/MCAL_Layer/MSSP/I2C/mcal_i2c.h
@@ -0,0 +1,25 @@
+/* 
+ * File:   mcal_i2c.h
+ * Author: Mohamed olwi
+ *
+ * Created on 31 December 2024, 19:43
+ */
+
+#ifndef MCAL_I2C_H
+#define	MCAL_I2C_H
+/*----------------------------Header Files------------------------------------*/
+#include "../../mcal_std_types.h"
+#include "../../GPIO/hal_gpio.h"
+#include <xc.h>
+#include "../../Interrupt/mcal_internal_interrupt.h"
+#include "../../Interrupt/mcal_interrupt_manager.h"
+#include "../../mcal_layer_cfg.h"
+/*----------------------------Macros Declarations-----------------------------*/
+
+/*----------------------------Macros Functions Declarations-------------------*/
+
+/*----------------------------DataTypes---------------------------------------*/
+
+/*----------------------------Function Prototypes-----------------------------*/
+#endif	/* MCAL_I2C_H */
+
